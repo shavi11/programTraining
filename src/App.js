6f012be674e1dadaaf5827733a3e231995df22a7
javier <shavi147@gmail.com>
2021-03-13 13:07:44 -0400

    Obtencion de usuarios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useEffect, useState } from 'react';
+import {Card, Image, Icon} from 'semantic-ui-react'
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
+
+  const [Login, setLogin]= useState('');
+  const [Avatar, setAvatar]= useState('');
+  const [Repos, setRepos]= useState('');
+
+  useEffect(  () => {
+    fetch("https://api.github.com/users/example").
+    then(res =>(res.json())).
+    then(data=>{
+      data.forEach(data => {
+        console.log(data)
+       });
+    })
+    
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className="navBar">
+          <h1>Github</h1>
+      </div>
+      <div className="card m-5">
+            <Card>
+                <Image src={Avatar} wrapped ui={false} />
+                    <Card.Content extra>
+                       <a>
+                        <Icon name='user' />
+                         {Login} 
+                       </a>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <a>
+                    <Icon name='user' />
+                         {Repos} Respository
+                      </a>
+                    </Card.Content>
+            </Card>
+        
+      </div>
+    </div>  
   );
+
+  
 }
+
+
 
 export default App;
